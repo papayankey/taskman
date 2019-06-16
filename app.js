fe508b20b1e5b740220b97d968e465c62913437f
ben bright <yankeybenneth@gmail.com>
2019-06-16 18:10:04 +0000

    integrated database with express

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const express = require("express");
 const path = require("path");
 
+const Database = require("./models/database");
+const schema = require("./models/schema");
 const userRoute = require("./routes/user");
 
+// environment variables
+require("dotenv").config();
 
-// app
-const app = express();
+// connect db (Dao = Database Access Object)
+const Dao = new Database(process.env.DB_PATH, schema);
 
-// middlewares
-app.disable("x-powered-by");
-app.use(express.static(path.resolve(__dirname, "public")));
-app.set("view engine", "pug");
+Dao.db.on("error", error => {
+  console.log(error.message);
+}).on("open", async () => {
+  console.log("Database connection successful");
+
+  // app
+  const app = express();
+
+  // middlewares
+  app.disable("x-powered-by");
+  app.use(express.static(path.resolve(__dirname, "public")));
+  app.set("view engine", "pug");
 
 
-// routes
-app.use("/", userRoute);
+  // routes
+  app.use("/", userRoute);
 
-// listen to port
-app.listen(4000, () => {
-  console.log("server running on http://localhost:4000");
-})
+
+  // listen to port
+  app.listen(process.env.APP_PORT, () => {
+    console.log(`server running on http://localhost:${process.env.APP_PORT}`);
+  })
+});
+

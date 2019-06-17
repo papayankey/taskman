d429769bcf4745e80f8725b1357fa07f7aa5ffdd
ben bright <yankeybenneth@gmail.com>
2019-06-17 00:45:26 +0000

    refactored content passed to pug

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,31 @@
 const express = require("express");
+const { register } = require("../controllers/user");
 
 // express router
 const router = express.Router();
 
 // GET
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { pageId: 'Home' });
 })
 
 router.get("/user/register", (req, res) => {
-  res.render("register");
+  res.render("register", {
+    pageTitle: "CREATE ACCOUNT",
+    pageId: 'Register'
+  });
 });
 
 router.get("/user/login", (req, res) => {
-  res.render("login");
+  res.render("login", {
+    pageTitle: "ACCESS ACCOUNT",
+    pageId: 'Login'
+  });
 });
 
 
 // POST
-router.post("/user/register", (req, res) => {
-  res.send("Registration Complete!");
-});
+router.post("/user/register", register);
 
 router.post("/user/login", (req, res) => {
   res.redirect("/");

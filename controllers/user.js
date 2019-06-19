4c94899c1e8bff4c3390b7454c6ccdbc5b9e3d45
ben bright <yankeybenneth@gmail.com>
2019-06-19 13:58:55 +0000

    added login controller with bcrypt hash compare

--- controllers/user.js
+++ controllers/user.js
@@ -22,5 +22,47 @@ module.exports = {
         errors: [{ field: "Email", message: "Email already exist" }]
       });
     }
+  },
+  login: async function(req, res) {
+    const models = req.app.locals.models;
+
+    let { email, password } = req.body;
+
+    try {
+      const renderError = () => {
+        res.status(500);
+        res.render("partials/content/login", {
+          title: "ACCESS ACCOUNT",
+          pageTitle: "Login",
+          errors: [{ message: "Email or password incorrect" }]
+        });
+      };
+
+      // get user from db by email
+      const user = await models.User.findByEmail(email);
+
+      // error message
+      if (!user) {
+        renderError();
+        return;
+      }
+
+      // compare passwords
+      const valid = await bcrypt.compare(password, user.password);
+
+      // error message
+      if (!valid) {
+        renderError();
+        return;
+      }
+      res.status(200);
+      res.redirect("../task");
+      return;
+    } catch (e) {
+      res.status(500).json({
+        status: 500,
+        message: "Email or password incorrect"
+      });
+    }
   }
 };

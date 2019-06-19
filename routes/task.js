c57519ad35bf9e56975ecf72a94b51c2b240b8e7
ben bright <yankeybenneth@gmail.com>
2019-06-19 14:00:36 +0000

    added task router

---
+++ routes/task.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { createTask } = require("../controllers/task");
+
+// express router
+const router = express.Router();
+
+// GET
+router.get("/", (req, res) => {
+  res.render("task");
+});
+
+module.exports = router;

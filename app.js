b02219c4dac35ccb4ddffa1e4068ef48af60cadb
ben bright <yankeybenneth@gmail.com>
2019-06-22 00:11:50 +0000

    added user session

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const session = require("express-session");
+const SQLiteStore = require("connect-sqlite3")(session);
 const path = require("path");
 const Database = require("./models/database");
 const schema = require("./models/schema");
@@ -9,10 +11,10 @@ const userRoute = require("./routes/user");
 const taskRoute = require("./routes/task");
 
 // environment variables
-require("dotenv").config();
+const { DB_PATH, APP_PORT, SESSION_NAME, SESSION_SECRET } = require("./config");
 
 // connect db (Dao = Database Access Object)
-const Dao = new Database(process.env.DB_PATH, schema);
+const Dao = new Database(DB_PATH, schema);
 
 Dao.db
   .on("error", error => {
@@ -30,10 +32,28 @@ Dao.db
     // set app locals to access models
     app.locals.models = { User };
 
+    // sqlite session store
+    const store = new SQLiteStore({
+      table: "userSessions",
+      dir: "./db"
+    });
+
     // middlewares
     app.disable("x-powered-by");
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use(
+      session({
+        store,
+        name: SESSION_NAME,
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+        }
+      })
+    );
     app.use(express.static(path.resolve(__dirname, "public")));
     app.set("view engine", "pug");
 
@@ -43,7 +63,7 @@ Dao.db
     app.use("/task", taskRoute);
 
     // listen to port
-    app.listen(process.env.APP_PORT, () => {
-      console.log(`server running on http://localhost:${process.env.APP_PORT}`);
+    app.listen(APP_PORT, () => {
+      console.log(`server running on http://localhost:${APP_PORT}`);
     });
   });

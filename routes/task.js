adba518593aa58ed47812e2661b7fe18ccad55f8
ben bright <yankeybenneth@gmail.com>
2019-06-22 00:14:21 +0000

    added session authentication

--- routes/task.js
+++ routes/task.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const { createTask } = require("../controllers/task");
 
+const modifyName = require("../helpers/firstToCapitalize");
+
 // express router
 const router = express.Router();
 
 // GET
-router.get("/", (req, res) => {
-  res.render("task", {
-    pageId: "Task"
-  });
+router.get("/", async (req, res) => {
+  if (req.session.user) {
+    let { name } = req.session.user;
+    name = await modifyName(name);
+    res.render("task", {
+      pageId: "Task",
+      user: {
+        name
+      }
+    });
+    return;
+  }
+
+  res.status(401);
+  res.redirect("/user/register");
 });
 
 module.exports = router;

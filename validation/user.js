f1b7bcd6973ad899644d612202d5ee7855bc8a78
ben bright <yankeybenneth@gmail.com>
2019-07-10 06:50:29 +0000

    refactored

--- validation/user.js
+++ validation/user.js
@@ -1,31 +1,39 @@
 const Joi = require("@hapi/joi");
 
-const loginSchema = Joi.object().keys({
-  email: Joi.string()
-    .email()
-    .required()
-    .label("Email"),
-  password: Joi.string()
-    .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
-    .required()
-    .label("Password")
-    .options({
-      language: {
-        string: {
-          regex: {
-            base: "minimum eight characters, at least one letter and one number"
-          }
+const email = Joi.string()
+  .email()
+  .required()
+  .label("Email");
+
+const password = Joi.string()
+  .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+  .required()
+  .label("Password")
+  .options({
+    language: {
+      string: {
+        regex: {
+          base: "minimum eight characters, at least one letter and one number"
         }
       }
-    })
+    }
+  });
+
+const name = Joi.string()
+  .min(8)
+  .max(30)
+  .required()
+  .label("Name");
+
+const loginSchema = Joi.object().keys({
+  email,
+  password
 });
 
-const registerSchema = loginSchema.keys({
-  name: Joi.string()
-    .min(8)
-    .max(30)
-    .required()
-    .label("Name")
+const registerSchema = Joi.object().keys({
+  name,
+  email,
+  password
 });
 
 module.exports = { loginSchema, registerSchema };

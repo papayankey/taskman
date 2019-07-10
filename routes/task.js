66e595fc64268af4dd11ae774ceb15ab1f2e6847
ben bright <yankeybenneth@gmail.com>
2019-07-10 06:50:13 +0000

    added more routes

--- routes/task.js
+++ routes/task.js
@@ -1,14 +1,48 @@
 const express = require("express");
-const { getAllTask, createTask, removeTask } = require("../controllers/task");
+const {
+  addTask,
+  destroyTask,
+  redoTask,
+  taskDone,
+  updateTask,
+  getAll,
+  getAllCompleted,
+  getAllActive,
+  destroyAllActive,
+  destroyAllCompleted,
+  destroyAllActiveToday,
+  destroyAllActiveYesterday,
+  destroyAllActivePast,
+  destroyAllCompletedToday,
+  destroyAllCompletedYesterday,
+  destroyAllCompletedPast
+} = require("../controllers/task");
 
 // express router
 const router = express.Router();
 
-// GET
-router.get("/", getAllTask);
-router.get("/r/:id", removeTask);
+// get
+router.get("/", getAllActive);
+router.get("/completed", getAllCompleted);
+router.get("/all", getAll);
 
-// POST
-router.post("/add", createTask);
+// post
+router.post("/add", addTask);
+router.post("/update", updateTask);
+
+// delete
+router.delete("/remove", destroyTask);
+router.delete("/r/all/active", destroyAllActive);
+router.delete("/r/all/completed", destroyAllCompleted);
+router.delete("/r/active/today", destroyAllActiveToday);
+router.delete("/r/active/yesterday", destroyAllActiveYesterday);
+router.delete("/r/active/past", destroyAllActivePast);
+router.delete("/r/completed/today", destroyAllCompletedToday);
+router.delete("/r/completed/yesterday", destroyAllCompletedYesterday);
+router.delete("/r/completed/past", destroyAllCompletedPast);
+
+// update
+router.patch("/complete", taskDone);
+router.patch("/redo", redoTask);
 
 module.exports = router;
